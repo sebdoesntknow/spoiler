90ca295b67532b8a32c8bfd4ec45d7253b86da06
seb <seba.grillo@gmail.com>
2015-06-25 17:32:10 -0300

    adding Title and Spoiler models

--- models.py
+++ models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Title(models.Model):
+    title_text = models.CharField(max_length=100)
+    sub_date = models.DateTimeField('Date Submitted')
+
+class Spoiler(models.Model):
+    title = models.ForeignKey(Title)
+    spoiler_text = models.TextField()
+    pub_date =  models.DateTimeField('Date Added')
+    votes = models.IntegerField(default=0)
