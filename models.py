2974c6403e813047732e8f6f2a235e97dca781ca
seb <seba.grillo@gmail.com>
2015-06-25 22:24:44 -0300

    index view done. Random title and its spoilers working

--- models.py
+++ models.py
@@ -6,8 +6,14 @@ class Title(models.Model):
     title_text = models.CharField(max_length=100)
     sub_date = models.DateTimeField('Date Submitted')
 
+    def __str__(self):
+        return self.title_text
+
 class Spoiler(models.Model):
     title = models.ForeignKey(Title)
     spoiler_text = models.TextField()
     pub_date =  models.DateTimeField('Date Added')
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.spoiler_text
--- views.py
+++ views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from random import randint
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Title, Spoiler
 
 def index(request):
-    return HttpResponse('This will be a spoilers website!')
+    title = get_object_or_404(Title, pk=randint(1, Title.objects.count()))
+    spoiler_choices = [sp.id for sp in title.spoiler_set.all()]
+    spoiler = title.spoiler_set.get(pk=spoiler_choices[randint(0, len(spoiler_choices) -1)])
+    
+    return render(request, 'web_spoiler/index.html', {'title': title.title_text, 'spoiler': spoiler})
+
