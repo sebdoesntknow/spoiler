2307040c430c6aa00b52ee3066719b6a3d338ee1
seb <seba.grillo@gmail.com>
2015-07-05 22:36:09 -0300

    home page working unittest

--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from web_spoiler.views import index
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_index(self):
+        found = resolve('/spoiler/')
+        self.assertEqual(found.func, index)
 
 
